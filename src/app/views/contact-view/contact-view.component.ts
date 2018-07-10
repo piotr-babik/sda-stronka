2b192905e5e4521879cce6410767bf390dec51fb
Szymon Rybka <artifexet@gmail.com>
2018-07-10 11:35:03 +0200

    dodano formularz kontaktowy

--- src/app/views/contact-view/contact-view.component.ts
+++ src/app/views/contact-view/contact-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../../services/login.service';
 
 @Component({
   selector: 'app-contact-view',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactViewComponent implements OnInit {
 
-  constructor() { }
+  formData;
+
+  constructor(loginService: LoginService) { 
+    this.formData = {
+      email: loginService.getUserLogin(),
+      interests: [
+        "angular",
+        "kawa"
+      ]
+    };
+  }
 
   ngOnInit() {
   }
 
+  submitContactForm() {
+    console.log(this.formData);
+  }
 }
